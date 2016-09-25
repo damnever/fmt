d5a69ea6f9ec197b43ebffac05aff672600b5d55
damnever <dxc.wolf@gmail.com>
2016-09-25 22:24:37 +0800

    utils

---
+++ makefile
@@ -0,0 +1,11 @@
+clean: clean-pyc
+test: run-tests
+
+clean-pyc:
+	find . -type f -name '*.pyc' -exec rm -f {} +
+	find . -type f -name '*.pyo' -exec rm -f {} +
+	find . -type f -name '*.~' -exec rm -f {} +
+	find . -type d -name '__pycache__' -exec rm -rf {} +
+
+run-tests:
+	python -m unittest discover pigar/tests/ -t . -v
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import codecs
+
+from setuptools import setup, find_packages
+
+
+version = ''
+with open('fmt/__init__.py', 'r') as f:
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        f.read(), re.M).group(1)
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='fmt',
+    version=version,
+    description=('f-strings(Python 3.6) style literal string interpolation.'),
+    long_description=long_description,
+    url='https://github.com/damnever/fmt',
+    author='damnever',
+    author_email='dxc.wolf@gmail.com',
+    license='The BSD 3-Clause License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='requirements tool',
+    packages=find_packages(),
+    include_package_data=True,
+)
