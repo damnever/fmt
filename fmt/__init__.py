76e3f4080db0263234ac8c08d04cd0539e0c718f
damnever <dxc.wolf@gmail.com>
2017-01-19 22:16:59 +0800

    bump version

--- fmt/__init__.py
+++ fmt/__init__.py
@@ -11,7 +11,7 @@ import sys
 from .fmt import Fmt
 
 
-version = __version__ = '0.3.0'
+version = __version__ = '0.3.1'
 version_info = [int(num) for num in version.split('.')]
 __author__ = 'damnever (X.C Dong)'
 __email__ = 'dxc.wolf@gmail.com'
